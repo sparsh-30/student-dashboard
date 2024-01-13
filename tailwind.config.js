a2703f41d1a86974250192361ec7bd4e5d4d69db
Sparsh Gupta <gsparsh04@gmail.com>
2024-01-13 16:36:48 +0530

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#978CD0',
+      'secondary': '#DCD9EF',
+      'background': '#fff',
+      'textPrimary': '#000',
+      'textSecondary': '#666666',
+    },
+  },
+  plugins: [],
+}
+
